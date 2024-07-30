8b013db4f71425a7860c01c5fc0d9c7a75dafda4
Guti <gutifer666@gmail.com>
2024-07-30 23:46:03 +0200

    createReturnsOneQuestionWithPercentageOfQuestionsEqualTo50and2Questions test passed

--- src/test/java/trade/javiergutierrez/quizharvestserver/application/CreateTestControllerImplTest.java
+++ src/test/java/trade/javiergutierrez/quizharvestserver/application/CreateTestControllerImplTest.java
@@ -60,4 +60,30 @@ class CreateTestControllerImplTest {
 
 
     }
+
+    @Test
+    void    createReturnsOneQuestionWithPercentageOfQuestionsEqualTo50and2Questions() {
+        // Given parameters
+        Subject subject = Subject.DATABASES;
+        Evaluation evaluation = Evaluation.SECOND;
+        int percentageOfQuestions = 50;
+
+        // And a list of 2 questions from the database
+        QuestionDao questionDao1 = new QuestionDao();
+        QuestionDao questionDao2 = new QuestionDao();
+        List<QuestionDao> questionDaoList = List.of(questionDao1, questionDao2);
+        when(questionRepository.findByEvaluationAndSubject(evaluation, subject)).thenReturn(questionDaoList);
+
+        // And a list of 2 questions
+        Question question1 = new Question();
+        Question question2 = new Question();
+        List<Question> questionList = List.of(question1, question2);
+        when(listQuestionDaoToListQuestion.convert(questionDaoList)).thenReturn(questionList);
+
+        // When a test is created
+        trade.javiergutierrez.quizharvestserver.domain.Test result = createTestControllerImpl.create(subject, evaluation, percentageOfQuestions);
+
+        // Then the test contains one question
+        assertEquals(1, result.getQuestions().size());
+    }
 }
