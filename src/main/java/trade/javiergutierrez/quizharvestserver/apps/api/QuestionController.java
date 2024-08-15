70af2ab8d0ad3cf78c48e96ecf948e440cbc7ee7
Guti <gutifer666@gmail.com>
2024-08-15 18:53:39 +0200

    added bySubjectAndEvaluation in Question Service

--- src/main/java/trade/javiergutierrez/quizharvestserver/apps/api/QuestionController.java
+++ src/main/java/trade/javiergutierrez/quizharvestserver/apps/api/QuestionController.java
@@ -1,20 +1,19 @@
 package trade.javiergutierrez.quizharvestserver.apps.api;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import trade.javiergutierrez.quizharvestserver.contexts._shared.domain.Question;
-import trade.javiergutierrez.quizharvestserver.contexts._shared.domain.QuestionRepository;
+import trade.javiergutierrez.quizharvestserver.contexts._shared.infrastructure.persistence.MySQLQuestionRepository;
 import trade.javiergutierrez.quizharvestserver.contexts.management.application.QuestionService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/questions")
 public class QuestionController {
     private final QuestionService questionService;
 
-    public QuestionController(QuestionRepository questionRepository) {
+    public QuestionController(MySQLQuestionRepository questionRepository) {
         this.questionService = new QuestionService(questionRepository);
     }
 
@@ -22,5 +21,11 @@ public class QuestionController {
     public ResponseEntity<Void> createQuestion(@RequestBody Question question) {
         questionService.createQuestion(question);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{subject}/{evaluation}")
+    public ResponseEntity<List<Question>> readQuestionsBySubjectAndEvaluation(@PathVariable String subject, @PathVariable String evaluation) {
+        List<Question> questions = questionService.readQuestionsBySubjectAndEvaluation(subject, evaluation);
+        return ResponseEntity.ok(questions);
     }
 }
--- src/main/java/trade/javiergutierrez/quizharvestserver/contexts/management/application/QuestionService.java
+++ src/main/java/trade/javiergutierrez/quizharvestserver/contexts/management/application/QuestionService.java
@@ -1,19 +1,28 @@
 package trade.javiergutierrez.quizharvestserver.contexts.management.application;
 
 import org.springframework.stereotype.Service;
+import trade.javiergutierrez.quizharvestserver.contexts._shared.domain.Evaluation;
 import trade.javiergutierrez.quizharvestserver.contexts._shared.domain.Question;
 import trade.javiergutierrez.quizharvestserver.contexts._shared.domain.QuestionRepository;
-import trade.javiergutierrez.quizharvestserver.contexts._shared.infrastructure.persistence.MySQLQuestionRepository;
+import trade.javiergutierrez.quizharvestserver.contexts._shared.domain.Subject;
+
+import java.util.List;
 
 @Service
 public class QuestionService {
     QuestionRepository questionRepository;
 
-    public QuestionService(MySQLQuestionRepository questionRepository) {
+    public QuestionService(QuestionRepository questionRepository) {
         this.questionRepository = questionRepository;
     }
 
     public void createQuestion(Question question) {
         questionRepository.save(question);
+    }
+
+    public List<Question> readQuestionsBySubjectAndEvaluation(String subject, String evaluation) {
+        Subject subjectEnum = Subject.valueOf(subject.toUpperCase());
+        Evaluation evaluationEnum = Evaluation.valueOf(evaluation.toUpperCase());
+        return questionRepository.bySubjectAndEvaluation(subjectEnum, evaluationEnum);
     }
 }
