a2d10209cd90889c7f60c835f9864db408a3f18a
Guti <gutifer666@gmail.com>
2024-08-15 18:01:06 +0200

    added MysqlQuestionRepository

--- src/main/java/trade/javiergutierrez/quizharvestserver/contexts/management/application/QuestionService.java
+++ src/main/java/trade/javiergutierrez/quizharvestserver/contexts/management/application/QuestionService.java
@@ -3,12 +3,13 @@ package trade.javiergutierrez.quizharvestserver.contexts.management.application;
 import org.springframework.stereotype.Service;
 import trade.javiergutierrez.quizharvestserver.contexts._shared.domain.Question;
 import trade.javiergutierrez.quizharvestserver.contexts._shared.domain.QuestionRepository;
+import trade.javiergutierrez.quizharvestserver.contexts._shared.infrastructure.persistence.MySQLQuestionRepository;
 
 @Service
 public class QuestionService {
     QuestionRepository questionRepository;
 
-    public QuestionService(QuestionRepository questionRepository) {
+    public QuestionService(MySQLQuestionRepository questionRepository) {
         this.questionRepository = questionRepository;
     }
 
