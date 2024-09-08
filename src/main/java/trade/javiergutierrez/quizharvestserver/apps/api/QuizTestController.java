307720aab05b6d5cfecf148b833ed216223eab0b
Guti <gutifer666@gmail.com>
2024-09-08 17:20:50 +0200

    cross origin from local host

--- src/main/java/trade/javiergutierrez/quizharvestserver/apps/api/QuizTestController.java
+++ src/main/java/trade/javiergutierrez/quizharvestserver/apps/api/QuizTestController.java
@@ -1,6 +1,7 @@
 package trade.javiergutierrez.quizharvestserver.apps.api;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import trade.javiergutierrez.quizharvestserver.contexts.quiz.domain.Test;
 import trade.javiergutierrez.quizharvestserver.contexts._shared.infrastructure.persistence.MySQLQuestionRepository;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class QuizTestController {
 
     private final TestMaker testMaker;
